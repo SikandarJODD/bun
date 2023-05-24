ec7b58647b29e50646890d7b6902a4ca0d078c27
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-05-25 04:12:31 +0530

    Added Aniamtion

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { fade, fly } from "svelte/transition";
+  let visible = false;
+  setTimeout(() => {
+    visible = true;
+  }, 800);
+</script>
+
+<section class="bg-gray-50 relative">
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+  >
+    {#if visible}
+      {#key visible}
+        <div
+          class="absolute hidden md:block border-2 w-1/4 left-10 h-3/5 border-gray-900 top-24 rounded-lg bg-yellow-300"
+          transition:fly={{ y: 200, duration: 800, delay: 500 }}
+        >
+          <div
+            class="absolute box border-2 w-3/4 h-10 top-5 left-6 border-gray-900 rounded-lg bg-blue-300"
+            transition:fly={{ x: -50, duration: 500, delay: 800 }}
+          />
+          <div
+            class="absolute box border-2 w-10 h-10 top-5 left-[315px] border-gray-900 rounded-full bg-blue-300"
+            transition:fly={{ x: 50, duration: 500, delay: 800 }}
+          />
+          <div
+            class="absolute box border-2 w-[328px] h-48 top-20 left-6 border-gray-900 rounded-lg bg-emerald-300"
+            transition:fly={{ y: 50, duration: 900, delay: 1200 }}
+          />
+        </div>
+      {/key}
+    {/if}
+
+    <div class="mx-auto max-w-xl text-center">
+      <h1 class="text-3xl font-extrabold sm:text-5xl text-sky-600">
+        <span
+          class="md:border-2 border-gray-900 px-5 py-2 bg-sky-100 rounded-xl"
+        >
+          Bunny UI
+        </span>
+        <strong class="font-extrabold text-gray-900 sm:block mt-4">
+          Build Valuable Project
+        </strong>
+      </h1>
+
+      <p class="mt-4 sm:text-xl/relaxed text-gray-400">
+        This Components are made with <span class="text-orange-500 opacity-80">
+          Svelte</span
+        >. It's easy to use and customize.
+        <strong class="text-gray-500">Just Copy Paste</strong> the Components.
+      </p>
+
+      <div class="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          class="block w-full rounded border-sky-500 border-2 text-gray-900 px-12 py-3 text-sm font-medium shadow hover:bg-sky-100 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          href="/get-started"
+        >
+          Explore
+        </a>
+
+        <a
+          class="block w-full rounded px-12 py-3 border-2 text-sm font-medium text-sky-900 shadow hover:border-2 hover:border-gray-900 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+          href="/about"
+        >
+          Components
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,155 @@
 <script>
-  import "../app.postcss";
+  import "../app.css";
+  import Logo from "../logo.png";
+  import { page } from "$app/stores";
+  $: RouteId = $page.route.id;
+  let data = {
+    title: "Bunny UI",
+    imgSrc: Logo,
+    navs: [
+      {
+        nv: "Home",
+        link: "/",
+        isLarge: false,
+      },
+      {
+        nv: "Components",
+        link: "/components",
+        isLarge: true,
+        smallnvs: [
+          {
+            nv: "Button",
+            link: "/components/button",
+          },
+          {
+            nv: "Card",
+            link: "/components/card",
+          },
+          {
+            nv: "Navbars",
+            link: "/components/navbars",
+          },
+          {
+            nv: "Grid Based",
+            link: "/components/grid-based",
+          },
+        ],
+        smallnvs2: [
+          {
+            nv: "Hero Section",
+            link: "/components/hero",
+          },
+          {
+            nv: "Footer",
+            link: "/components/footer",
+          },
+        ],
+      },
+      {
+        nv: "About Us",
+        link: "/about",
+        isLarge: false,
+      },
+    ],
+  };
 </script>
 
+<div
+  class="navbar bg-gray-900 border-b-2 border-slate-400 md:flex sticky top-0"
+  id="nav_height"
+>
+  <div class="navbar-start">
+    <div class="dropdown">
+      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          /></svg
+        >
+      </label>
+      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+      <ul
+        tabindex="0"
+        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <!-- Phone -->
+        <li><a href="/">Home</a></li>
+        <li><a href="/components">Components</a></li>
+        <li><a href="/about">About Us</a></li>
+      </ul>
+    </div>
+    <a class="btn btn-ghost normal-case text-xl" href="/">
+      <img src={data.imgSrc} width="45" alt="" />
+      <span class="px-2 text-2xl text-sky-500 font-bold">{data.title}</span></a
+    >
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <!-- Laptop -->
+      {#each data.navs as nav}
+        {#if nav.isLarge == true}
+          <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+          <li tabindex="0">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a>
+              {nav.nv}
+              <svg
+                class="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                ><path
+                  d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
+                /></svg
+              >
+            </a>
+            <ul>
+              <div class=" flex flex-row mt-4 bg-base-200 p-2 rounded-xl border-2">
+                <ul class="p-2 bg-base-200">
+                  {#each nav.smallnvs as smallnv}
+                    <li><a href={smallnv.link} class="p-3 hover:text-sky-500">{smallnv.nv}</a></li>
+                  {/each}
+                </ul>
+                <div class="divider divider-horizontal lg:divider-horizontal" />
+                <ul class="p-2 bg-base-200">
+                  {#each nav.smallnvs2 as smallnv}
+                    <li><a href={smallnv.link} class="p-3 hover:text-sky-500">{smallnv.nv}</a></li>
+                  {/each}
+                </ul>
+              </div>
+            </ul>
+          </li>
+        {:else}
+          <li>
+            <a href={nav.link} class="mx-1" class:act={RouteId == nav.link}
+              >{nav.nv}</a
+            >
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  </div>
+</div>
 <slot />
+
+<style lang="postcss">
+  .act {
+    color: rgb(17, 170, 252);
+    background: #46617dc4;
+  }
+  #nav_height {
+    min-height: 60px;
+    height: 60px !important;
+  }
+</style>
