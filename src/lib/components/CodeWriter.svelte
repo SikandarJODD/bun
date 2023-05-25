dcd78f84b930b84cd68bcb616ceadf0f8400909f
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-05-25 12:48:46 +0530

    Code Writer Added

---
+++ src/lib/components/CodeWriter.svelte
@@ -0,0 +1,125 @@
+<script>
+  let scrData = [
+    { smdata: "<script>", istrue: false, ispal: false },
+    {
+      smdata: " import Navbar from '$lib/components/Navbar';",
+      istrue: false,
+      ispal: true,
+    },
+    {
+      smdata: " import Footer from '$lib/components/Navbar';",
+      istrue: false,
+      ispal: true,
+    },
+    { smdata: "< /script>", istrue: false, ispal: false },
+    { smdata: " <main>", istrue: false, ispal: false },
+    { smdata: "    <Navbar />", istrue: false, ispal: true },
+    { smdata: "    <slot />", istrue: false, ispal: true },
+    { smdata: "    <Footer/>", istrue: false, ispal: true },
+    { smdata: "< /main>", istrue: false, ispal: false },
+  ];
+  $: scr5 = scrData[5].istrue;
+  $: scr6 = scrData[6].istrue;
+  $: scr7 = scrData[7].istrue;
+</script>
+
+<div class="flex flex-wrap flex-row">
+  <div class="box mockup-code">
+    {#each scrData as item, index}
+      <div
+        on:mouseenter={() => (item.istrue = true)}
+        on:mouseleave={() => (item.istrue = false)}
+        class="hover:bg-base-100 rounded-md px-2 {item.ispal === true
+          ? 'hover:text-sky-500'
+          : ''} "
+      >
+        <code
+          >{index + 1}.
+          <span class="{item.ispal === true ? 'pl-4 ' : 'pl=0'} "
+            >{item.smdata.replace(" ", "")}</span
+          ></code
+        >
+      </div>
+    {/each}
+  </div>
+  <div class="box">
+    <div class="sm_box">
+      <div class="header_content duration-100 {scr5 === true ? 'active' : ''} ">
+        {scr5 === true ? "Navbar " : ""}
+      </div>
+      <div class="slot_content {scr6 === true ? 'active' : ''}">
+        {@html scr6 === true
+          ? `Sikandar Bhide :  <span class="font-sans text-gray-900 pl-2">Juniour Fronted Developer</span>`
+          : ""}
+      </div>
+      <div class="footer_content {scr7 === true ? 'active' : ''}">
+        {@html scr7 === true ? "Footer" : ""}
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .box {
+    width: 43%;
+    margin: 70px auto;
+    padding: 20px 19px;
+    border: 1px solid #ffffff;
+    box-shadow: 7px 7px theme(colors.gray.900), 8px 8px #1fbcfa;
+  }
+  .sm_box {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #ffffff;
+    border-radius: 10px;
+    background: #56575824;
+  }
+  .header_content {
+    width: 95%;
+    height: 50px;
+    background: #686d73e0;
+    border-radius: 10px;
+    margin: 15px auto;
+    border: 1px solid transparent;
+    transition: all 0.2s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+  .slot_content {
+    width: 95%;
+    height: 100px;
+    background: #686d73e0;
+    border-radius: 10px;
+    margin: 15px auto;
+    border: 1px solid transparent;
+    transition: all 0.2s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.3rem;
+    font-weight: bold;
+  }
+  .footer_content {
+    width: 95%;
+    height: 50px;
+    background: #657282e0;
+    border-radius: 10px;
+    margin: 15px auto;
+    border: 1px solid transparent;
+    transition: all 0.2s ease-in-out;
+  }
+  .active {
+    background: #17868e;
+    color: #ffffff;
+    border: 1px solid #ffffff;
+    transition: all 0.2s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+  import CodeWriter from "../lib/components/CodeWriter.svelte";
   import Hero from "../lib/components/Hero.svelte";
 </script>
 
 <Hero />
+<CodeWriter />
