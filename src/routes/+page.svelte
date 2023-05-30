308f63b583e4f23ae01ded5b981e3553aba81f8f
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-05-31 04:17:34 +0530

    try icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <script>
   import CodeWriter from "../lib/components/CodeWriter.svelte";
   import Hero from "../lib/components/Hero.svelte";
+  // import logo from "../lib/assets/logo.svg";
+  import logo from "$lib/logo.png";
 </script>
 
+<svelte:head>
+  <title>Home Page</title>
+  <link rel="shortcut icon" href={logo} type="image/x-icon" />
+  <link rel="shortcut icon" href="../../lib/logo.png" type="image/x-icon" />
+  <meta name="og:image" content={logo} />
+  <meta name="og:title" content="Home Page | Bunny UI" />
+  <meta name="description" content="Copy Paste Components" />
+</svelte:head>
 <Hero />
 <CodeWriter />
